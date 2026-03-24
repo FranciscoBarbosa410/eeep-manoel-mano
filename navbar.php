c60507c9e2631654705f8212b570ec492ea4df98
Marcos Fellype Soares <soaresmarcosfellype@gmail.com>
2026-03-24 10:47:02 -0300

    implementando navbar

---
+++ navbar.php
@@ -0,0 +1,11 @@
+<nav>
+    <a href="index.php">
+        <img src="src/images/logo.png" alt="">    
+        EEEP MM
+    </a>
+    <ul>
+        <li><a href="noticias.php">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+</nav>
