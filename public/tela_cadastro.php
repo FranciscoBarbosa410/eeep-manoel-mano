8034a40d54eef1ebfd477447ebae92192ceb4725
Ana Lara <ana.sousa2324@aluno.ce.gov.br>
2026-03-24 10:26:17 -0300

    adicionando paginas

--- public/tela_cadastro.php
+++ public/tela_cadastro.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>tela_cadastro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+
+<div class="container">
+  <div class="esquerda">coluna01</div>
+  <div class="direita">coluna02</div>
+</div>
+ 
 </body>
 </html>
