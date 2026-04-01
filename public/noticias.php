4274ac7a556667f4647f74cd5fed1e8695c904c2
Ana Lara <ana.sousa2324@aluno.ce.gov.br>
2026-04-01 09:22:16 -0300

    Add files via upload

---
+++ public/noticias.php
@@ -0,0 +1,77 @@
+<!--<?php include'navbar.php';?>-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina-Inicial(noticias)</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="master-back">
+        <div class="noticias1">
+            <h1 class="text">Ultimas noticias</h1>
+            <!--painel de noticias-->
+            <div class="slider"> 
+                <button class="bnt-pass">mover</button>
+            <div class="slides">
+                <!---primeira noticia-->
+            <div class="slide">
+                <img src="" alt=""> <!---Imagem ainda sera decidida-->
+                    <div class="text-slide">
+                        <h2>Alguma-noticia</h2>
+                        <p>texto da noticia que ira ser apresentado</p>
+                    </div>
+            </div>
+                <!--segunda noticia-->
+            <div class="slide">
+                <img src="" alt=""> <!---Imagem ainda sera decidida-->
+                    <div class="text-slide">
+                        <h2>Alguma-noticia-02</h2>
+                        <p>texto da noticia que ira ser apresentado-02</p>
+                    </div>
+            </div>
+                 <!--terceira noticia-->
+            <div class="slide">
+                <img src="" alt=""> <!---Imagem ainda sera decidida-->
+                    <div class="text-slide">
+                        <h2>Alguma-noticia-03</h2>
+                        <p>texto da noticia que ira ser apresentado-03</p>
+                    </div>
+            </div>
+            <button class="bnt-previos">mover</button>
+            </div>
+            </div>
+
+            <!--noticias abaixo-->
+            <div class="noticias2">
+
+                <div class="grid-noticias">
+            <!--1-->
+            <div class="card-noticia">
+                <img src="" alt=""> <!---Imagem ainda sera decidida-->
+                <h3>card-noticia-01</h3>
+                <p>texto da noticia que ira ser apresentado-01</p>
+            </div>
+             <!--2-->
+            <div class="card-noticia">
+                <img src="" alt=""> <!---Imagem ainda sera decidida-->
+                <h3>card-noticia-02</h3>
+                <p>texto da noticia que ira ser apresentado-02</p>
+            </div>
+            <!--3-->
+            <div class="card-noticia">
+                <img src="" alt=""> <!---Imagem ainda sera decidida-->
+                <h3>card-noticia-03</h3>
+                <p>texto da noticia que ira ser apresentado-03</p>
+
+            </div>
+
+            </div>
+            </div>
+        </div>
+    </div>
+    <script src="src/js/script.js"> </script>
+</body>
+</html>
