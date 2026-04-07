42dc79231e2dc4a7f32b231a94855cc5f5ba70aa
Ana Lara <ana.sousa2324@aluno.ce.gov.br>
2026-04-07 10:55:39 -0300

    correcao frontend

---
+++ banco.php
@@ -0,0 +1,26 @@
+<?php
+    //Conexao com o BD
+    define('HOST', 'localhost');
+    define('USUARIO', 'root');
+    define('SENHA', '');
+    define('DB', 'eeep_mm');
+
+    $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Não foi possível conectar');
+
+    //Notícias
+    function buscar_noticias($conexao) {
+        $sqlBuscar = "SELECT * FROM noticias";
+        $resultado = mysqli_query($conexao, $sqlBuscar);
+
+        $noticias = [];
+
+        while($noticia = mysqli_fetch_assoc($resultado)) {
+            $noticias[] = $noticia;
+        }
+
+        return $noticias;
+    }
+
+
+
+?>
