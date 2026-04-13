0bf6799ac397f9d550d812736d8bf058f99b237d
Ana Lara <ana.sousa2324@aluno.ce.gov.br>
2026-04-12 23:35:00 -0300

    card-cursos

--- public/card_cursos.php
+++ public/card_cursos.php
@@ -1,11 +1,12 @@
-<div class="card-curso">
-<nav class="card-cursos">
+<link rel="stylesheet" href="src/css/card_cursos.css">
+<section class="curso">
+<div class="card-cursos">
     <ul>
-        <li><a href="enfermagem.php"><img src=""></a></li>
-        <li><a href="informatica.php"><img src=""></a></li>
-        <li><a href="ds.php"><img src="" alt=""></a></li>
-        <li><a href="administracao.php"><img src=""></a></li>
+        <li><a href="enfermagem.php"><img src="">a</a></li>
+        <li><a href="informatica.php"><img src="">b</a></li>
+        <li><a href="ds.php"><img src="" alt="">c</a></li>
+        <li><a href="administracao.php"><img src="">d</a></li>
         
     </ul>
-</nav>
 </div>
+</section>
--- public/tela_login.php
+++ public/tela_login.php
@@ -3,35 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link rel="stylesheet" href="./src/css/login.css">
 </head>
 <body>
-     <div class="master">
-        <div class="esquerda-tela">
+
+    <main class="master">
+
+        <!-- LADO ESQUERDO -->
+        <section class="esquerda-tela">
             <h1 class="titulo">Bem vindo a tela login!</h1>
-             <p>
-             ficamos felizes por ter você aqui. Se você tem acesso
-             preecha os campos para gerenciamento
+            <p>
+                Ficamos felizes por ter você aqui. Se você tem acesso,
+                preencha os campos para gerenciamento.
             </p>
-        </div>
-        <div class="direita-tela">
-            <div class="card-login">
-            <h1 class="titulo">login</h1>
-            <p class="sub">de administrador</p>
-            <form action="../app/login.php" method="post">    
-                <div class="text">
-                         <label for="nome">Nome</label>
-                         <input id="nome" type="text" name="nome" required>
-                </div>
-                <div class="text">
-                         <label for="senha">Senha</label>
-                         <input id="senha" type="password" name="senha" required>
-                </div>
-                 <button type="submit" class="btn-login">Login</button>
-            </form> 
-        </div>
-        </div>
-    </div>
+        </section>
+
+        <!-- LADO DIREITO -->
+        <section class="direita-tela">
+            <section class="card-login">
+                <h1 class="titulo">Login</h1>
+                <p class="sub">de administrador</p>
+
+                <form action="../app/login.php" method="post">    
+                    
+                    <section class="text">
+                        <label for="nome">Nome</label>
+                        <input id="nome" type="text" name="nome" required>
+                    </section>
+
+                    <section class="text">
+                        <label for="senha">Senha</label>
+                        <input id="senha" type="password" name="senha" required>
+                    </section>
+
+                    <button type="submit" class="btn-login">Login</button>
+                </form> 
+            </section>
+        </section>
+
+    </main>
+
 </body>
 </html>
