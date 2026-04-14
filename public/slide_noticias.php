5ebeb3bc6eaa7c35cacb272ea1ce9e20e792474a
Marcos Fellype Soares <soaresmarcosfellype@gmail.com>
2026-04-14 10:55:21 -0300

    arquivo do slide

---
+++ public/slide_noticias.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="src/css/noticias.css">
+<section>
+     <!-- painel de noticias -->
+        <div class="slider"> 
+            <button class="bnt-previos">‹</button>
+            <button class="bnt-pass">›</button>
+
+            <div class="slides">
+                <div class="slide">
+                    <img src="" alt="">
+                    <div class="text-slide">
+                        <h2>Alguma-noticia</h2>
+                        <p>texto da noticia</p>
+                    </div>
+                </div>
+
+                <div class="slide">
+                    <img src="" alt="">
+                    <div class="text-slide">
+                        <h2>Alguma-noticia-02</h2>
+                        <p>texto da noticia</p>
+                    </div>
+                </div>
+
+                <div class="slide">
+                    <img src="" alt="">
+                    <div class="text-slide">
+                        <h2>Alguma-noticia-03</h2>
+                        <p>texto da noticia</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+       <script src="src/js/script.js"></script>
+</section>
