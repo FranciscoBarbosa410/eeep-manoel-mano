9ba1e3ffcdaf337a57c1f5272de447fd9b3eec29
Marcos Fellype <soaresmarcosfellype@gmail.com>
2026-04-16 10:41:56 -0300

    correção navbar adm

--- public/adm/index_adm.php
+++ public/adm/index_adm.php
@@ -1,4 +1,5 @@
 <?php include('../../app/verifica_login.php'); ?>
+<?php include'navbar_adm.php'; ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -6,10 +7,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <link rel="stylesheet" href="../src/css/navbar_adm.css">
    
 </head>
 <body>
-    <?php include'navbar_adm.php'; ?>
     <?php 
         echo "Admin logado com sucesso!";
         
--- public/adm/navbar_adm.php
+++ public/adm/navbar_adm.php
@@ -1,12 +1,15 @@
-<link rel="stylesheet" href="../src/css/navbar_adm.css">
+
 <nav>
     <a href="index.php">
-        <img src="src/images/logo.png">    
+        <img src="../src/images/logo.png">    
         EEEP MM
     </a>
     <ul>
+        <li><a href="../cursos.php">Cursos</a></li>
+        <li><a href="../profissionais.php">Profissionais</a></li>
         <li><a href="cad_noticias.php">Notícias</a></li>
         <li><a href="cad_classificados.php">Classificados</a></li>
+        <li><a href="../sobre.php">Sobre nós</a></li>
         <li><a href="../../app/logout.php">Logout</a></li>
         
    
