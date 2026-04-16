5b43ec761a247800d7b21ea0e9170c2e1348ada7
Marcos Fellype <soaresmarcosfellype@gmail.com>
2026-04-16 10:13:48 -0300

    correção css

--- public/card_cursos.php
+++ public/card_cursos.php
@@ -1,12 +1,12 @@
 <link rel="stylesheet" href="src/css/card_cursos.css">
+
 <section class="curso">
-<div class="card-cursos">
-    <ul>
-        <li><a href="enfermagem.php"><img src="">a</a></li>
-        <li><a href="informatica.php"><img src="">b</a></li>
-        <li><a href="ds.php"><img src="" alt="">c</a></li>
-        <li><a href="administracao.php"><img src="">d</a></li>
-        
-    </ul>
-</div>
+    <div class="card-cursos">
+        <ul>
+            <li><a href="enfermagem.php"><img src="">A</a></li>
+            <li><a href="informatica.php"><img src="">B</a></li>
+            <li><a href="ds.php"><img src="">C</a></li>
+            <li><a href="administracao.php"><img src="">D</a></li>
+        </ul>
+    </div>
 </section>
