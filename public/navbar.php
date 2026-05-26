4d284ac3d5bcda93c7945610926da21c8e6ddd8e
Ana Lara <ana.sousa2324@aluno.ce.gov.br>
2026-05-25 21:14:32 -0300

    implementacao-sidebar

--- public/navbar.php
+++ public/navbar.php
@@ -1,19 +1,83 @@
 <header id="header">
+
     <nav class="navbar">
+
+        <div class="nav-top">
+
+    <div class="nav-left">
+
+        <button class="menu-toggle" id="menu-toggle">
+            ☰
+        </button>
+
         <a href="index.php" class="home-link">
-            <img src="src/images/logo.png" alt="Logo EEEP">    
+            <img src="src/images/logo.png" alt="Logo EEEP">
             EEEP Manoel Mano
         </a>
-        <ul>
-            <li><a href="cursos.php">Cursos</a></li>
-            <li><a href="noticias.php">Notícias</a></li>
-            <!-- <li><a href="profissionais.php">Profissionais</a></li> -->
-            <!-- <li><a href="sobre.php">Sobre nós</a></li> -->
-            <li><a href="#">Profissionais</a></li>
-            <li><a href="#">Sobre nós</a></li>
-            <li>
-                <a href="tela_login.php">Login</a>
-            </li>
-        </ul>
+
+    </div>
+
+    <div class="nav-right">
+        <a href="tela_login.php" class="login-btn">
+             login
+        </a>
+    </div>
+
+</div>
+
+        <aside class="sidebar" id="sidebar">
+            <ul>
+                <li><a href="cursos.php">Cursos</a></li>
+                <li><a href="noticias.php">Notícias</a></li>
+
+                <!-- <li><a href="profissionais.php">Profissionais</a></li> -->
+                <!-- <li><a href="sobre.php">Sobre nós</a></li> -->
+
+                <li><a href="#">Profissionais</a></li>
+                <li><a href="#">Sobre nós</a></li>
+
+                <li>
+                    <a href="tela_login.php">Login</a>
+                </li>
+            </ul>
+        </aside>
+
     </nav>
+
 </header>
+
+<script>
+
+const toggle = document.getElementById("menu-toggle");
+const sidebar = document.getElementById("sidebar");
+
+
+toggle.addEventListener("click", () => {
+
+    sidebar.classList.toggle("active");
+
+    if(sidebar.classList.contains("active")){
+        toggle.innerHTML = "✖";
+    }else{
+        toggle.innerHTML = "☰";
+    }
+
+});
+
+document.addEventListener("click", (e) => {
+
+    const clicouDentroSidebar = sidebar.contains(e.target);
+    const clicouNoBotao = toggle.contains(e.target);
+
+    if(
+        !clicouDentroSidebar &&
+        !clicouNoBotao &&
+        sidebar.classList.contains("active")
+    ){
+        sidebar.classList.remove("active");
+        toggle.innerHTML = "☰";
+    }
+
+});
+
+</script>
